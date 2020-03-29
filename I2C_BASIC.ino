c62b2c7f8471b9bbe86f63bac321251120a8f74a
Maccony <maccony@mail.ru>
2020-03-29 16:54:14 +0800

    Update I2C_BASIC.ino

--- I2C_BASIC.ino
+++ I2C_BASIC.ino
@@ -2,21 +2,45 @@
 //(PC5) A5 - SCLK
 //(PC4) A4 - SDIO
 #define address_SI4703 0x10
-#define START_TWSR 0x08
+#define START_TWI 0x08
 
 void setup() {
   Serial.begin(9600); //устанавливаем последовательное соединение
-
   TWBR=0x20; //скорость передачи (при 8 мГц получается 100 кГц)
-  I2C_StartCondition();
 }
 
 void loop() {
-    Serial.println(TWCR);
+    I2C_StartCondition(); // отправляем в шину "СТАРТ"
+    Serial.println(TWSR, HEX); // отображаем статусный регистр TWSR
+    delay(1000);
+    I2C_SRA_R();
+    Serial.println(TWSR, HEX); // отображаем статусный регистр TWSR
+    delay(1000);
+    I2C_READ();
+    I2C_StopCondition();
     delay(1000);
 }
 
 void I2C_StartCondition(void) {
     TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN); // отправка "СТАРТ"
     while(!(TWCR&(1<<TWINT)));  // ожидаем пока "СТАРТ" отправится
+    if ((TWSR & 0xF8) != START_TWI) Serial.println("ERROR START");
+}
+
+void I2C_StopCondition(void) {
+    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+}
+
+void I2C_SRA_R(void) { // выдаем на шину пакет SLA-R
+    TWDR = (address_SI4703<<1)|1;
+    TWCR = (1<<TWINT)|(1<<TWEN);
+    while(!(TWCR & (1<<TWINT)));
+}
+
+void I2C_READ(void) { /*считываем данные с подтверждением*/
+    TWCR = (1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+    while(!(TWCR & (1<<TWINT)));
+    data = TWDR;
+    Serial.println(data , HEX); // отображаем полученый байт
+    delay(5000);
 }
