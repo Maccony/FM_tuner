d74874bc6a037ef1d4e94ada4a718917d5a27e3f
Maccony <maccony@mail.ru>
2020-03-30 18:51:26 +0800

    Update I2C_BASIC.ino

--- I2C_BASIC.ino
+++ I2C_BASIC.ino
@@ -5,14 +5,14 @@ byte registers_FM[32]; // выделяем массив для 16 регистр
 
 void setup() {
   Serial.begin(9600); //устанавливаем последовательное соединение
-  reset_Si4703(); // сброс Si4703;
+  reset_Si4703(); // сброс si4703 (теперь регистры доступны на запись и чтение)
   TWBR=0x20; // задаем скорость передачи (при 8 мГц получается 100 кГц)
 }
 void loop() {
     send_START(); // отправляем в шину "СТАРТ"
     send_SLA_X(1); // отправляем в шину SLA(адрес ведомого) и 1-Read или 0-Write
-    for(byte count=0; count<32; count++) {
-        busTWI_READ();
+    for(byte count=0; count<32; count++) { // читаем данные из регистров Si4703
+        read_DATA();
         registers_FM[count] = TWDR;
     }
     send_STOP();
@@ -25,16 +25,23 @@ void reset_Si4703(void) {
     delay(1);} // время что бы выйти из сброса
 
 void send_START(void) {
-    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // сбрасываем бит прерывания TWINT, активируем шину TWI установкой TWEN, формируем "СТАРТ" установив TWSTA
-    while(!(TWCR&(1<<TWINT)));}  // ожидаем пока "СТАРТ" отправится
+    TWCR |= (1<<TWSTA); // формируем "СТАРТ" установив TWSTA
+    bus_READY();}
 
 void send_STOP(void) {TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);} // формируем "СТОП" установив TWSTO
 
 void send_SLA_X(byte orRW) { // выдаем на шину пакет SLA
     TWDR = (0x10<<1)|orRW; // в TWDR загружаем 0х10 - адрес Si7703
-    TWCR = (1<<TWINT)|(1<<TWEN);
-    while(!(TWCR & (1<<TWINT)));}
+    bus_READY();}
 
-void busTWI_READ(void) { /*считываем данные с подтверждением*/
-    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA); // выставив TWEA ждем подтверждение (ACK) от ведомого
+void read_DATA(void) { /*считываем данные с подтверждением*/
+    TWCR |= (1<<TWEA);
+    bus_READY();}
+
+void bus_READY(void) {
+    TWCR |= (1<<TWINT)|(1<<TWEN); // сбрасываем бит прерывания TWINT, активируем шину TWI установкой TWEN
+    while(!(TWCR&(1<<TWINT)));} // ожидаем пока "СТАРТ" отправится
+
+void write_DATA(void) { /*считываем данные с подтверждением*/
+    TWCR = (1<<TWINT)|(1<<TWEN); // выставив TWEA ждем подтверждение (ACK) от ведомого
     while(!(TWCR & (1<<TWINT)));}
