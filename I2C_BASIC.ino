a2d644f810c08f0b42276e66185a17c86d0d6677
Maccony <maccony@mail.ru>
2020-04-01 14:49:32 +0800

    read write func

--- I2C_BASIC.ino
+++ I2C_BASIC.ino
@@ -7,33 +7,12 @@ void setup() {
   Serial.begin(9600); //устанавливаем последовательное соединение
   reset_Si4703(); // сброс Si4703;
   TWBR=0x20; // задаем скорость передачи (при 8 мГц получается 100 кГц)
+  readRegs();
+  for(byte count=0; count<28; count++) Serial.println(registers_FM[count], HEX);
 }
-void loop() {
-    send_START(); // отправляем в шину "СТАРТ"
-    send_SLA_R(); // отправляем в шину SLA(адрес ведомого) и 1-Read или 0-Write
-    for(byte count=0; count<28; count++) {
-        busTWI_READ();
-        registers_FM[count] = TWDR;}
-    send_STOP();
-    for(byte count=0; count<28; count++) Serial.println(registers_FM[count], HEX);
-}
+void loop() {}
 
-void send_START(void) { // формируем "СТАРТ" установив TWSTA
-    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
-    while(!(TWCR & (1<<TWINT)));}  // ожидаем пока "СТАРТ" отправится
-
-void send_SLA_R() { // выдаем на шину пакет SLA
-    TWDR = 0B00100001; // в TWDR загружаем 0х10 - адрес Si7703
-    TWCR = (1<<TWINT)|(1<<TWEN); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
-    while(!(TWCR & (1<<TWINT)));} // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
-
-void busTWI_READ(void) { // считываем данные с подтверждением
-    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
-    while(!(TWCR & (1<<TWINT)));} // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
-
-void send_STOP(void) {TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);} // формируем "СТОП" установив TWSTO
-
-void readRegs() {
+void readRegs(void) {
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
     while(!(TWCR & (1<<TWINT)));  // ожидаем пока "СТАРТ" отправится
     TWDR = 0B00100001; // в TWDR загружаем 0х10 - адрес Si7703
@@ -42,12 +21,12 @@ void readRegs() {
     for(byte count = 0; count < 28; count++) {
         TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
         while(!(TWCR & (1<<TWINT))); // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
-        registers_FM[count] = TWDR;
-    }
+        registers_FM[count] = TWDR;}
+    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO); // формируем "СТОП" установив TWSTO
 }
 
 //запись в si4703 начинается с регистра 0x02. Но мы не должны писать в регистры 0x08 и 0x09
-void writeRegs() {
+void writeRegs(void) {
     // формируем "СТАРТ" установив TWSTA
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
     while(!(TWCR & (1<<TWINT)));  // ожидаем пока "СТАРТ" отправится
