ae574ca8f1fb6d49e615fe8b6e69243efca85ac9
Maccony <maccony@mail.ru>
2020-04-02 21:26:20 +0800

    Update FM_mini_massiv.ino

--- FM_mini_massiv.ino
+++ FM_mini_massiv.ino
@@ -1,16 +1,17 @@
 // ARDUINO PB1 - Si4702 RST, (PC5) A5 - SCLK, (PC4) A4 - SDIO
 byte registers_FM[12]; // задаем переменную для считывания регистров Si4703
+byte statusRSSI;
 
 void setup() {
     reset_Si4703(); // сброс si4703 (теперь регистры доступны на запись и чтение)
     TWBR=0x20; // задаем скорость передачи (при 8 мГц получается 100 кГц)
     readRegs();
-    registers_FM[10] |= (1<<7); //запуск внутреннего генератора, включение бита XOSCEN (регистр 0х07h Test1)
+    registers_FM[10] |= (1<<7); //запуск внутреннего генератора, включение бита 15 -> XOSCEN (регистр 0х07h Test1)
     writeRegs();
     delay(500);
     readRegs();
-    registers_FM[0] = 0x40; //регистр 0х02 (старший байт) бит 14 -> DMUTE = 1 (Mute disable)
-    registers_FM[1] = 0x01; //регистр 0х02 (младший байт) бит 1 -> ENABLE = 1 (Powerup Enable)
+    registers_FM[0] |= (1<<6); //регистр 0х02 (старший байт) бит 14 -> DMUTE = 1 (Mute disable)
+    registers_FM[1] |= (1<<0); //регистр 0х02 (младший байт) бит 0 -> ENABLE = 1 (Powerup Enable)
     writeRegs();
     delay(110);
     readRegs();
@@ -22,11 +23,15 @@ void setup() {
 
     gotoChannel(171); //171, 181, 139, 189
 }
-void loop() {}
+void loop() {
+    if(PINB & (1 << PINB4)) { //если на входе PB4 значение HIGH (кнопка нажата), то...
+        delay(500);
+    }
+}
 
 void gotoChannel(byte newChannel){
 	readRegs(); //считываем регистры si4703
-	registers_FM[2] = 0x80;//регистр 0х03h старший байт (6 байт массива), бит D15 -> TUNE = 1
+	registers_FM[2] |= (1<<7);//регистр 0х03h старший байт, бит D15 -> TUNE = 1
 	registers_FM[3] = newChannel;//регистр 0х03h младьший байт (7 байт массива), CHANNEL
 	writeRegs();//записываем регистры
    	//These steps come from AN230 page 20 rev 0.5
@@ -35,38 +40,39 @@ void gotoChannel(byte newChannel){
     //когда настройка на волну закончится, в регистре 0Ah, бит STC установится в 1
     while(1) { // бесконечный цикл, прерывание через break
         readRegs();
-        if( (registers_FM[0] & (1<<6)) != 0) break;} //Tuning complete!
-    registers_FM[2] = 0; //очистим вит TUNE в регистре 03h когда частота настроена
+        if( (statusRSSI & (1<<6)) != 0) break;} //Tuning complete!
+    registers_FM[2] &= ~(1<<7); //очистим бит TUNE в регистре 03h когда частота настроена
     writeRegs();
     while(1) { //Wait for the si4703 to clear the STC as well
         readRegs();
-        if( (registers_FM[2] & (1<<6)) == 0) break;} //Tuning complete!
+        if((statusRSSI & (1<<6)) == 0) break;} // Tuning complete!
 }
 //ФУНКЦИЯ ЧТЕНИЯ ИЗ РЕГИСТРОВ (считывает весь набор регистров управления Si4703 (от 0x00 до 0x0F)
 //чтение из Si4703 начинается с регистра 0x0A и до 0x0F, затем возвращается к 0x00 далее до 0х09
 void readRegs(void) {
-    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // отправляем условие "СТАРТ" в шину TWI (регистр TWI в ATmega328)
-    while(!(TWCR & (1<<TWINT)));  // ожидаем пока "СТАРТ" отправится
-    TWDR = 0B00100001; // в TWDR загружаем 0х10 - адрес Si7703 и флаг чтения (1)
-    TWCR = (1<<TWINT)|(1<<TWEN); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
-    while(!(TWCR & (1<<TWINT))); // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
+    connect_TWI(0B00100001); // обращаемся к устройству 0х10 (адрес Si7703) и добавляем флаг чтения (1) 10000|1
     for(byte count = 0; count < 28; count++) {
         TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA); // активируем TWEA чтоб подтвердить прием байта
         while(!(TWCR & (1<<TWINT))); // ожидаем когда TWINT обнулится аппаратно
+        if(count == 0) statusRSSI = TWDR;
         if(count > 15) registers_FM[count - 16] = TWDR;} // в массив сохраняем только байты регистров 0х02 - 0х07
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);} // формируем "СТОП" установив TWSTO
 //запись в si4703 начинается с регистра 0x02. Но мы не должны писать в регистры 0x08 и 0x09
 void writeRegs(void) {
-    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // формируем "СТАРТ" установив TWSTA
-    while(!(TWCR & (1<<TWINT)));  // ожидаем пока "СТАРТ" отправится
-    TWDR = 0B00100000; // в TWDR загружаем 0х10 - адрес Si7703 и флаг записи (0) 10000|0
-    TWCR = (1<<TWINT)|(1<<TWEN); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
-    while(!(TWCR & (1<<TWINT))); // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
+    connect_TWI(0B00100000); // обращаемся к устройству 0х10 (адрес Si7703) и добавляем флаг записи (0) 10000|0
     for(byte count = 0; count < 12; count++) { // запись в si4703 начинается с регистра 0x02. Но нет записи в регистры 0x08 и 0x09
         TWDR = registers_FM[count];
         TWCR = (1<<TWINT)|(1<<TWEN); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
         while(!(TWCR & (1<<TWINT)));} // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);} // формируем "СТОП" установив TWSTO
+
+void connect_TWI(byte address) {
+    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA); // отправляем условие "СТАРТ" в шину TWI (регистр TWI в ATmega328)
+    while(!(TWCR & (1<<TWINT)));  // ожидаем пока "СТАРТ" отправится
+    TWDR = address; // в TWDR загружаем 0х10 - адрес Si7703 и флаг чтения (1)
+    TWCR = (1<<TWINT)|(1<<TWEN); // сбрасываем бит прерывания TWINT (ставим в 1), активируем шину TWI установкой TWEN
+    while(!(TWCR & (1<<TWINT)));} // ожидаем когда TWINT обнулится аппаратно (закончится выполнение операции отправки SLA)
+
 void reset_Si4703(void) {// сброс si4703 (теперь регистры доступны на запись и чтение)
     DDRB |= (1 << DDB1); // через регистр направления DDRB назначаем вывод PB1 выходным (OUTPUT), ставим бит №1 в HIGH
     PORTB |= (1 << PORTB1); // через регистр данных порта PORTB устанавливаем на выводе PB1 значение HIGH
